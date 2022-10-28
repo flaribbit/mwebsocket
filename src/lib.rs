080b070818eed1a43d171550d2a0dd2322ca2944
flaribbit <1149761294@qq.com>
2022-10-28 18:47:29 +0800

    可以 很棒 学到很多 稍后写博客

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,46 @@
 use mlua::prelude::*;
 use std::collections::VecDeque;
+use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
+use tungstenite::{connect, Message};
+use tungstenite::{protocol::WebSocket, stream::MaybeTlsStream};
+use url::Url;
 
 struct Client {
     messages: VecDeque<String>,
+    socket: Arc<Mutex<Option<WebSocket<MaybeTlsStream<TcpStream>>>>>,
 }
+
 impl Client {
     fn new() -> Self {
         Self {
             messages: VecDeque::new(),
+            socket: Arc::new(Mutex::new(None)),
+        }
+    }
+    fn connect(&mut self, url: String) {
+        let (socket, _response) = connect(Url::parse(&url).unwrap()).expect("Can't connect");
+        let socket = Arc::new(Mutex::new(Some(socket)));
+        self.socket = socket.clone();
+        std::thread::spawn(move || loop {
+            if let Some(socket) = socket.lock().unwrap().as_mut() {
+                if let Ok(msg) = socket.read_message() {
+                    if let Message::Text(msg) = msg {
+                        println!("Received: {}", msg);
+                    }
+                }
+            }
+            std::thread::sleep(std::time::Duration::from_millis(10));
+        });
+    }
+    fn send(&mut self, text: String) {
+        if let Some(socket) = self.socket.lock().unwrap().as_mut() {
+            socket.write_message(Message::Text(text)).unwrap();
+        }
+    }
+    fn close(&mut self) {
+        if let Some(socket) = self.socket.lock().unwrap().as_mut() {
+            socket.close(None).unwrap();
         }
     }
 }
@@ -15,8 +48,19 @@ impl Client {
 impl LuaUserData for Client {
     fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
         methods.add_method_mut("poll", |_, this, ()| Ok(this.messages.pop_front()));
-        methods.add_method_mut("new", |_, _this, _url: String| Ok(Client::new()));
+        methods.add_method_mut("connect", |_, this, url: String| Ok(this.connect(url)));
+        methods.add_method_mut("send", |_, this, text: String| Ok(this.send(text)));
+        methods.add_method_mut("close", |_, this, ()| Ok(this.close()));
     }
+}
+
+fn new_client(_: &Lua, _: ()) -> LuaResult<Client> {
+    Ok(Client::new())
+}
+
+fn sleep(_: &Lua, time: f64) -> LuaResult<()> {
+    std::thread::sleep(std::time::Duration::from_millis((time * 1000.0) as u64));
+    Ok(())
 }
 
 fn hello(_: &Lua, name: String) -> LuaResult<()> {
@@ -28,7 +72,8 @@ fn hello(_: &Lua, name: String) -> LuaResult<()> {
 fn my_module(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("hello", lua.create_function(hello)?)?;
-    exports.set("client", lua.create_userdata(Client::new())?)?;
+    exports.set("newClient", lua.create_function(new_client)?)?;
+    exports.set("sleep", lua.create_function(sleep)?)?;
     Ok(exports)
 }
 
@@ -37,11 +82,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() -> LuaResult<()> {
+    fn simple_test() -> LuaResult<()> {
         let lua = Lua::new();
-        lua.globals().set("lib", my_module(&lua).unwrap())?;
-        lua.load("lib.hello('123')").eval()?;
-        assert!(true);
+        lua.globals()
+            .set("newClient", lua.create_function(new_client)?)?;
+        lua.globals().set("sleep", lua.create_function(sleep)?)?;
+        lua.load(
+            r#"
+            local client = newClient()
+            print(client)
+            client:connect("ws://127.0.0.1:8080/live")
+            sleep(10)
+        "#,
+        )
+        .exec()?;
         Ok(())
     }
 }
