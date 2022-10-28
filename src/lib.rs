90461f768b31792b3b9dd4e130baf79eefb1e180
flaribbit <1149761294@qq.com>
2022-10-28 23:38:57 +0800

    消息队列完成 添加json库

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use mlua::prelude::*;
+use mlua::LuaSerdeExt;
 use std::collections::VecDeque;
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
@@ -7,26 +8,31 @@ use tungstenite::{protocol::WebSocket, stream::MaybeTlsStream};
 use url::Url;
 
 struct Client {
-    messages: VecDeque<String>,
+    messages: Arc<Mutex<VecDeque<String>>>,
     socket: Arc<Mutex<Option<WebSocket<MaybeTlsStream<TcpStream>>>>>,
 }
 
 impl Client {
     fn new() -> Self {
         Self {
-            messages: VecDeque::new(),
+            messages: Arc::new(Mutex::new(VecDeque::new())),
             socket: Arc::new(Mutex::new(None)),
         }
     }
     fn connect(&mut self, url: String) {
         let (socket, _response) = connect(Url::parse(&url).unwrap()).expect("Can't connect");
         let socket = Arc::new(Mutex::new(Some(socket)));
+        let messages = self.messages.clone();
         self.socket = socket.clone();
         std::thread::spawn(move || loop {
             if let Some(socket) = socket.lock().unwrap().as_mut() {
                 if let Ok(msg) = socket.read_message() {
-                    if let Message::Text(msg) = msg {
-                        println!("Received: {}", msg);
+                    match msg {
+                        Message::Text(text) => {
+                            messages.lock().unwrap().push_back(text);
+                        }
+                        Message::Binary(_) => {}
+                        _ => {}
                     }
                 }
             }
@@ -43,11 +49,14 @@ impl Client {
             socket.close(None).unwrap();
         }
     }
+    fn poll(&mut self) -> Option<String> {
+        self.messages.lock().unwrap().pop_front()
+    }
 }
 
 impl LuaUserData for Client {
     fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_method_mut("poll", |_, this, ()| Ok(this.messages.pop_front()));
+        methods.add_method_mut("poll", |_, this, ()| Ok(this.poll()));
         methods.add_method_mut("connect", |_, this, url: String| Ok(this.connect(url)));
         methods.add_method_mut("send", |_, this, text: String| Ok(this.send(text)));
         methods.add_method_mut("close", |_, this, ()| Ok(this.close()));
@@ -58,22 +67,26 @@ fn new_client(_: &Lua, _: ()) -> LuaResult<Client> {
     Ok(Client::new())
 }
 
-fn sleep(_: &Lua, time: f64) -> LuaResult<()> {
-    std::thread::sleep(std::time::Duration::from_millis((time * 1000.0) as u64));
-    Ok(())
+fn json_parse(lua: &Lua, text: String) -> LuaResult<LuaValue> {
+    lua.to_value(&serde_json::from_str::<serde_json::Value>(&text).unwrap())
 }
 
-fn hello(_: &Lua, name: String) -> LuaResult<()> {
-    println!("hello, {}!", name);
+fn json_stringify(_: &Lua, value: LuaValue) -> LuaResult<String> {
+    Ok(serde_json::to_string(&value).unwrap())
+}
+
+fn sleep(_: &Lua, time: f64) -> LuaResult<()> {
+    std::thread::sleep(std::time::Duration::from_millis((time * 1000.0) as u64));
     Ok(())
 }
 
 #[mlua::lua_module]
 fn my_module(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
-    exports.set("hello", lua.create_function(hello)?)?;
     exports.set("newClient", lua.create_function(new_client)?)?;
     exports.set("sleep", lua.create_function(sleep)?)?;
+    exports.set("json_parse", lua.create_function(json_parse)?)?;
+    exports.set("json_stringify", lua.create_function(json_stringify)?)?;
     Ok(exports)
 }
 
@@ -84,18 +97,23 @@ mod tests {
     #[test]
     fn simple_test() -> LuaResult<()> {
         let lua = Lua::new();
-        lua.globals()
-            .set("newClient", lua.create_function(new_client)?)?;
-        lua.globals().set("sleep", lua.create_function(sleep)?)?;
+        lua.globals().set("lib", my_module(&lua)?)?;
         lua.load(
             r#"
-            local client = newClient()
+            local client = lib.newClient()
             print(client)
             client:connect("ws://127.0.0.1:8080/live")
-            sleep(10)
+            while true do
+                while true do
+                    local msg = client:poll()
+                    if not msg then break end
+                    print('message:', msg)
+                end
+                lib.sleep(0.1)
+            end
         "#,
         )
-        .exec()?;
+        .eval()?;
         Ok(())
     }
 }
