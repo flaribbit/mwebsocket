ec3991d7485fa128c52838aee9110fe8aea7b956
flaribbit <1149761294@qq.com>
2023-10-25 22:13:31 +0800

    更优雅的错误管理

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![warn(clippy::unwrap_used)]
 use mlua::prelude::*;
 use std::collections::BTreeMap;
 use std::net::TcpStream;
@@ -22,6 +23,17 @@ struct Promise {
     data: Arc<Mutex<Option<HttpResponse>>>,
 }
 
+trait LockUnwrap<T> {
+    fn lock_unwrap(&self) -> std::sync::MutexGuard<T>;
+}
+
+impl<T> LockUnwrap<T> for Arc<Mutex<T>> {
+    fn lock_unwrap(&self) -> std::sync::MutexGuard<T> {
+        #[allow(clippy::unwrap_used)]
+        self.lock().unwrap()
+    }
+}
+
 const EVENT_PREFIX: &str = "@";
 
 impl Client {
@@ -42,35 +54,39 @@ impl Client {
                 None => connect(url),
             };
             match res {
-                Ok((s, _)) => *socket.lock().unwrap() = Some(s),
+                Ok((s, _)) => *socket.lock_unwrap() = Some(s),
                 Err(e) => {
-                    push_event(&tx, &format!("error: {e}"));
+                    push_error(&tx, &e.to_string());
                     push_event(&tx, "close");
                     return;
                 }
             }
             push_event(&tx, "open");
             loop {
-                if let Some(socket) = socket.lock().unwrap().as_mut() {
-                    let close = check_message(&tx, socket.read_message());
+                if let Some(socket) = socket.lock_unwrap().as_mut() {
+                    let close = check_message(&tx, socket.read());
                     if close {
                         break;
                     }
                 }
                 std::thread::sleep(std::time::Duration::from_millis(10));
             }
-            *socket.lock().unwrap() = None;
+            *socket.lock_unwrap() = None;
             push_event(&tx, "close");
         });
     }
     fn send(&mut self, text: String) {
-        if let Some(socket) = self.socket.lock().unwrap().as_mut() {
-            socket.write_message(Message::Text(text)).unwrap();
+        if let Some(socket) = self.socket.lock_unwrap().as_mut() {
+            if let Err(e) = socket.write(Message::Text(text)) {
+                push_error(&self.tx, &e.to_string());
+            }
         }
     }
     fn close(&mut self) {
-        if let Some(socket) = self.socket.lock().unwrap().as_mut() {
-            socket.close(None).unwrap();
+        if let Some(socket) = self.socket.lock_unwrap().as_mut() {
+            if let Err(e) = socket.close(None) {
+                push_error(&self.tx, &e.to_string());
+            }
         }
     }
     fn poll(&mut self) -> Option<String> {
@@ -88,11 +104,14 @@ fn connect_with_headers(
     ),
     tungstenite::Error,
 > {
-    let uri: http::Uri = url.parse().unwrap();
+    let uri: http::Uri = url.parse()?;
+    let host = uri.host().ok_or(tungstenite::Error::Url(
+        tungstenite::error::UrlError::EmptyHostName,
+    ))?;
     let key = tungstenite::handshake::client::generate_key();
     let request = http::Request::builder()
-        .uri(url)
-        .header("Host", uri.host().unwrap())
+        .uri(&uri)
+        .header("Host", host)
         .header("Connection", "Upgrade")
         .header("Upgrade", "websocket")
         .header("Sec-WebSocket-Version", "13")
@@ -100,8 +119,7 @@ fn connect_with_headers(
     let request = headers
         .iter()
         .fold(request, |req, [k, v]| req.header(k, v))
-        .body(())
-        .unwrap();
+        .body(())?;
     connect(request)
 }
 
@@ -109,21 +127,27 @@ fn push_event(tx: &Sender<String>, event: &str) {
     tx.send(EVENT_PREFIX.to_string() + event).unwrap();
 }
 
+fn push_error(tx: &Sender<String>, error: &str) {
+    push_event(tx, &("error: ".to_string() + error));
+}
+
 fn check_message(tx: &Sender<String>, incoming: tungstenite::Result<Message>) -> bool {
     match incoming {
         Ok(message) => {
             if let Message::Text(text) = message {
-                tx.send(text).unwrap();
+                if let Err(e) = tx.send(text) {
+                    push_error(tx, &e.to_string());
+                }
             }
             false
         }
         Err(tungstenite::Error::AlreadyClosed) => {
-            push_event(tx, "error: Connection already closed");
+            push_error(tx, "Connection already closed");
             true
         }
         Err(tungstenite::Error::ConnectionClosed) => true,
         Err(error) => {
-            push_event(tx, &format!("error: {error}"));
+            push_error(tx, &error.to_string());
             false
         }
     }
@@ -142,7 +166,7 @@ fn http_get(_: &Lua, (url, headers): (String, Option<Vec<[String; 2]>>)) -> LuaR
     let pdata = res.data.clone();
     std::thread::spawn(move || {
         let response = request.call().unwrap();
-        pdata.lock().unwrap().replace(HttpResponse {
+        pdata.lock_unwrap().replace(HttpResponse {
             status: response.status() as i32,
             headers: [].into(),
             body: "".into(),
@@ -153,7 +177,7 @@ fn http_get(_: &Lua, (url, headers): (String, Option<Vec<[String; 2]>>)) -> LuaR
 
 impl Promise {
     fn poll(&mut self) -> Option<HttpResponse> {
-        self.data.lock().unwrap().take()
+        self.data.lock_unwrap().take()
     }
 }
 
@@ -195,24 +219,22 @@ fn new_client(_: &Lua, _: ()) -> LuaResult<Client> {
     Ok(Client::new())
 }
 
+fn to_lua_error<E: ToString>(e: E) -> LuaError {
+    LuaError::RuntimeError(e.to_string())
+}
+
 fn json_parse(lua: &Lua, text: String) -> LuaResult<LuaValue> {
-    lua.to_value(&serde_json::from_str::<serde_json::Value>(&text).unwrap())
+    lua.to_value(&serde_json::from_str::<serde_json::Value>(&text).map_err(to_lua_error)?)
 }
 
 fn json_stringify(_: &Lua, value: LuaValue) -> LuaResult<String> {
-    Ok(serde_json::to_string(&value).unwrap())
-}
-
-fn sleep(_: &Lua, time: f64) -> LuaResult<()> {
-    std::thread::sleep(std::time::Duration::from_millis((time * 1000.0) as u64));
-    Ok(())
+    serde_json::to_string(&value).map_err(to_lua_error)
 }
 
 #[mlua::lua_module]
 fn mwebsocket(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("newClient", lua.create_function(new_client)?)?;
-    exports.set("sleep", lua.create_function(sleep)?)?;
     exports.set("jsonParse", lua.create_function(json_parse)?)?;
     exports.set("jsonStringify", lua.create_function(json_stringify)?)?;
     exports.set("get", lua.create_function(http_get)?)?;
